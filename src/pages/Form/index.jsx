6661e18dc797d6bbee251772e8db4251b0ab8466
Pablo Barral <pablo.barral.kts@gmail.com>
2022-06-21 17:06:19 -0300

    Feat: structured form page

--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -4,7 +4,25 @@ function Form() {
 
   return (
     <div className="main">
-      
+      <form className='modal'>
+        <header>
+            <img alt='Header Image'/>
+            <h1>Intern Sign Up</h1>
+        </header>
+        <Input placeholder='Name' field='Full Name' req={true}/>
+        <div>
+          <Input placeholder='foo@bar.com' field='Email' req={true} type='email'/>
+          <Input field='Password' req={true} type='password'/>
+        </div>
+        <div>
+          <Input placeholder='(83) 00000-0000' field='Phone' type='phone'/>
+          <Input field='Birthday' req={true} type='date'/>
+        </div>
+        <span>
+          <Input type='checkbox'/>
+          <Button/>
+        </span>
+      </form>
     </div>
   )
 }
