1258cfcfe2d149279aabc45da3581d0a36f2b437
Pablo Barral <pablo.barral.kts@gmail.com>
2022-06-23 13:04:33 -0300

    Style: styled checkbox component

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,15 +1,18 @@
 import styled from 'styled-components'
+import './styles.css'
 
 function Checkbox (props){
 
 return(
-    <div className={props.className}>
+    <div className={`${props.className} container`}>
         <span>
-            <input name={props.name}  
+            <input name={props.name}
+            className='checkbox'  
             type='checkbox'
             onChange={ e => {  
                 localStorage.setItem(`${props.name}`, e.target.checked)
             }} />
+            <span className="checkmark"></span>
             <label htmlFor={props.name}>{props.label}</label>
         </span>
         { !props.valid && <p className='errorMsg'>{props.msg}</p>}
@@ -20,9 +23,6 @@ return(
 const StyledCheckbox = styled(Checkbox)`
 display: flex;
 flex-direction: column;
-input{
-
-}
 .errorMsg{
     color: red;
 }
