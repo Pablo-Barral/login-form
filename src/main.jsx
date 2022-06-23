dd310fbfae998843838887c0b7ce532b2abcdec8
Pablo Barral <pablo.barral.kts@gmail.com>
2022-06-23 08:34:18 -0300

    Feat: routes in place

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client'
 import Form from './pages/Form'
 import './index.css'
 import Success from './pages/Success'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Form/>
-    <Success/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Form/>} />
+        <Route path='/success' element={<Success/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
